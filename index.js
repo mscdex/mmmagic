7bf528d8618ba61a8874fbdd22d2772646973141
Brian White <mscdex@mscdex.net>
2012-07-26 16:55:14 -0400

    Use path.join() instead of depending on availability of path.sep
    Closes #3

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var path = require('path');
 
 var Magic = require('./build/Release/magic');
 
-Magic.setFallback(__dirname + path.sep + 'magic' + path.sep + 'magic');
+var fbpath = path.join(__dirname, 'magic', 'magic');
+Magic.setFallback(fbpath);
 
 module.exports = {
   Magic: Magic.Magic,
-  MAGIC_NONE: 0x000000, /* No flags */
+  MAGIC_NONE: 0x000000, /* No flags (default for Windows) */
   MAGIC_DEBUG: 0x000001, /* Turn on debugging */
-  MAGIC_SYMLINK: 0x000002, /* Follow symlinks (default) */
-  MAGIC_COMPRESS: 0x000004, /* Check inside compressed files */
+  MAGIC_SYMLINK: 0x000002, /* Follow symlinks (default for *nix) */
   MAGIC_DEVICES: 0x000008, /* Look at the contents of devices */
   MAGIC_MIME_TYPE: 0x000010, /* Return the MIME type */
   MAGIC_CONTINUE: 0x000020, /* Return all matches */
@@ -21,7 +21,6 @@ module.exports = {
   MAGIC_MIME: (0x000010|0x000400), /*(MAGIC_MIME_TYPE|MAGIC_MIME_ENCODING)*/
   MAGIC_APPLE: 0x000800, /* Return the Apple creator and type */
 
-  MAGIC_NO_CHECK_COMPRESS: 0x001000, /* Don't check for compressed files */
   MAGIC_NO_CHECK_TAR: 0x002000, /* Don't check for tar files */
   MAGIC_NO_CHECK_SOFT: 0x004000, /* Don't check magic entries */
   MAGIC_NO_CHECK_APPTYPE: 0x008000, /* Don't check application type */
