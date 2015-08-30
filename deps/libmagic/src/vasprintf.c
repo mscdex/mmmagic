3cb3e6d946331557dff560575abac7eaaaf626a0
Brian White <mscdex@mscdex.net>
2015-08-30 18:52:18 -0400

    deps: use simpler vasprintf implementation on Windows

--- deps/libmagic/src/vasprintf.c
+++ deps/libmagic/src/vasprintf.c
@@ -1,66 +1,27 @@
 // XXX: change by mscdex
-// from:
-// http://www.redantigua.com/c-ex-beginner-concat-strings-asprintf.html
+// from mingw-w64-crt project
 
 #include <errno.h>
-#include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#ifndef VA_COPY
-# ifdef HAVE_VA_COPY
-#  define VA_COPY(dest, src) va_copy(dest, src)
-# else
-#  ifdef HAVE___VA_COPY
-#   define VA_COPY(dest, src) __va_copy(dest, src)
-#  else
-#   define VA_COPY(dest, src) (dest) = (src)
-#  endif
-# endif
-#endif
-
-#define INIT_SZ 128
-
-int
-vasprintf(char **str, const char *fmt, va_list ap)
-{
-        int ret = -1;
-        va_list ap2;
-        char *string, *newstr;
-        size_t len;
-
-        VA_COPY(ap2, ap);
-        if ((string = malloc(INIT_SZ)) == NULL)
-                goto fail;
-
-        ret = vsnprintf(string, INIT_SZ, fmt, ap2);
-        if (ret >= 0 && ret < INIT_SZ) { /* succeeded with initial alloc */
-                *str = string;
-        } else if (ret == INT_MAX || ret < 0) { /* Bad length */
-                goto fail;
-        } else {        /* bigger than initial, realloc allowing for nul */
-                len = (size_t)ret + 1;
-                if ((newstr = realloc(string, len)) == NULL) {
-                        free(string);
-                        goto fail;
-                } else {
-                        va_end(ap2);
-                        VA_COPY(ap2, ap);
-                        ret = vsnprintf(newstr, len, fmt, ap2);
-                        if (ret >= 0 && (size_t)ret < len) {
-                                *str = newstr;
-                        } else { /* failed with realloc'ed string, give up */
-                                free(newstr);
-                                goto fail;
-                        }
-                }
-        }
-        va_end(ap2);
-        return (ret);
-
-fail:
-        *str = NULL;
-        errno = ENOMEM;
-        va_end(ap2);
-        return (-1);
+int vasprintf(char ** ret, const char * format, va_list ap) {
+  int len;
+  /* Get Length */
+  len = _vsnprintf(NULL, 0, format, ap);
+  if (len < 0)
+    return -1;
+  /* +1 for \0 terminator. */
+  *ret = malloc(len + 1);
+  /* Check malloc fail*/
+  if (!*ret) {
+    errno = ENOMEM;
+    return -1;
+  }
+  /* Write String */
+  _vsnprintf(*ret, len + 1, format, ap);
+  /* Terminate explicitly */
+  (*ret)[len] = '\0';
+  return len;
 }
