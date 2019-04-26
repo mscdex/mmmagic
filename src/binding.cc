78decce5ff93db8f713a5bb0041ce31ef705a1b9
Brian White <mscdex@mscdex.net>
2019-04-26 12:19:24 -0400

    src: fix build issues with node v12

--- src/binding.cc
+++ src/binding.cc
@@ -15,9 +15,35 @@
 using namespace node;
 using namespace v8;
 
-struct Baton {
+class DetectRequest : public Nan::AsyncResource {
+public:
+  DetectRequest(Local<Function> callback_, const char* magic_source_,
+                size_t source_len_, bool source_is_path_, int flags_)
+    : Nan::AsyncResource("mmmagic:DetectRequest"),
+      magic_source(magic_source_),
+      source_len(source_len_),
+      source_is_path(source_is_path_),
+      flags(flags_) {
+    callback.Reset(callback_);
+
+    request.data = this;
+    free_error = true;
+    error_message = nullptr;
+    result = nullptr;
+  }
+
+  ~DetectRequest() {
+    callback.Reset();
+    data_buffer.Reset();
+    if (data_is_path)
+      free(data);
+    if (free_error)
+      free(error_message);
+    free((void*)result);
+  }
+
   uv_work_t request;
-  Nan::Callback* callback;
+  Nan::Persistent<Function> callback;
 
   char* data;
   size_t data_len;
@@ -30,7 +56,6 @@ struct Baton {
   bool source_is_path;
   int flags;
 
-  bool error;
   bool free_error;
   char* error_message;
 
@@ -64,7 +89,7 @@ public:
       mflags = flags;
     }
 
-    Magic(Handle<Object> buffer, int flags) {
+    Magic(Local<Object> buffer, int flags) {
       mgc_buffer.Reset(buffer);
       mgc_buffer_len = Buffer::Length(buffer);
       msource = Buffer::Data(buffer);
@@ -99,22 +124,22 @@ public:
 
       if (args.Length() > 1) {
         if (args[1]->IsInt32())
-          magic_flags = args[1]->Int32Value();
+          magic_flags = Nan::To<int32_t>(args[1]).FromJust();
         else
           return Nan::ThrowTypeError("Second argument must be an integer");
       }
 
       if (args.Length() > 0) {
         if (args[0]->IsString()) {
-          String::Utf8Value str(args[0]->ToString());
+          Nan::Utf8String str(args[0]);
           char* path = strdup((const char*)(*str));
           obj = new Magic(path, magic_flags);
         } else if (Buffer::HasInstance(args[0])) {
           obj = new Magic(args[0].As<Object>(), magic_flags);
         } else if (args[0]->IsInt32()) {
-          magic_flags = args[0]->Int32Value();
+          magic_flags = Nan::To<int32_t>(args[0]).FromJust();
           obj = new Magic(nullptr, magic_flags);
-        } else if (args[0]->IsBoolean() && !args[0]->BooleanValue()) {
+        } else if (args[0]->IsBoolean() && !Nan::To<bool>(args[0]).FromJust()) {
           char* path = strdup(magic_getpath(nullptr, 0/*FILE_LOAD*/));
           obj = new Magic(path, magic_flags);
         } else {
@@ -143,24 +168,18 @@ public:
 
       Local<Function> callback = Local<Function>::Cast(args[1]);
 
-      String::Utf8Value str(args[0]->ToString());
+      Nan::Utf8String str(args[0]);
 
-      Baton* baton = new Baton();
-      baton->error = false;
-      baton->free_error = true;
-      baton->error_message = nullptr;
-      baton->request.data = baton;
-      baton->callback = new Nan::Callback(callback);
-      baton->data = strdup((const char*)*str);
-      baton->data_is_path = true;
-      baton->magic_source = obj->msource;
-      baton->source_len = obj->mgc_buffer_len;
-      baton->source_is_path = obj->mgc_buffer.IsEmpty();
-      baton->flags = obj->mflags;
-      baton->result = nullptr;
+      DetectRequest* detect_req = new DetectRequest(callback,
+                                                    obj->msource,
+                                                    obj->mgc_buffer_len,
+                                                    obj->mgc_buffer.IsEmpty(),
+                                                    obj->mflags);
+      detect_req->data = strdup((const char*)*str);
+      detect_req->data_is_path = true;
 
       int status = uv_queue_work(uv_default_loop(),
-                                 &baton->request,
+                                 &detect_req->request,
                                  Magic::DetectWork,
                                  (uv_after_work_cb)Magic::DetectAfter);
       assert(status == 0);
@@ -182,24 +201,18 @@ public:
       Local<Function> callback = Local<Function>::Cast(args[1]);
       Local<Object> buffer_obj = args[0].As<Object>();
 
-      Baton* baton = new Baton();
-      baton->error = false;
-      baton->free_error = true;
-      baton->error_message = nullptr;
-      baton->request.data = baton;
-      baton->callback = new Nan::Callback(callback);
-      baton->data = Buffer::Data(buffer_obj);
-      baton->data_len = Buffer::Length(buffer_obj);
-      baton->data_buffer.Reset(buffer_obj);
-      baton->data_is_path = false;
-      baton->magic_source = obj->msource;
-      baton->source_len = obj->mgc_buffer_len;
-      baton->source_is_path = obj->mgc_buffer.IsEmpty();
-      baton->flags = obj->mflags;
-      baton->result = nullptr;
+      DetectRequest* detect_req = new DetectRequest(callback,
+                                                    obj->msource,
+                                                    obj->mgc_buffer_len,
+                                                    obj->mgc_buffer.IsEmpty(),
+                                                    obj->mflags);
+      detect_req->data = Buffer::Data(buffer_obj);
+      detect_req->data_len = Buffer::Length(buffer_obj);
+      detect_req->data_buffer.Reset(buffer_obj);
+      detect_req->data_is_path = false;
 
       int status = uv_queue_work(uv_default_loop(),
-                                 &baton->request,
+                                 &detect_req->request,
                                  Magic::DetectWork,
                                  (uv_after_work_cb)Magic::DetectAfter);
       assert(status == 0);
@@ -208,53 +221,50 @@ public:
     }
 
     static void DetectWork(uv_work_t* req) {
-      Baton* baton = static_cast<Baton*>(req->data);
+      DetectRequest* detect_req = static_cast<DetectRequest*>(req->data);
       const char* result;
-      struct magic_set* magic = magic_open(baton->flags
+      struct magic_set* magic = magic_open(detect_req->flags
                                            | MAGIC_NO_CHECK_COMPRESS
                                            | MAGIC_ERROR);
 
       if (magic == nullptr) {
 #if NODE_MODULE_VERSION <= 0x000B
-        baton->error_message = strdup(uv_strerror(
-                                        uv_last_error(uv_default_loop())));
+        detect_req->error_message =
+          strdup(uv_strerror(uv_last_error(uv_default_loop())));
 #else
 // XXX libuv 1.x currently has no public cross-platform function to convert an
 //     OS-specific error number to a libuv error number. `-errno` should work
 //     for *nix, but just passing GetLastError() on Windows will not work ...
 # ifdef _MSC_VER
-        baton->error_message = strdup(uv_strerror(GetLastError()));
+        detect_req->error_message = strdup(uv_strerror(GetLastError()));
 # else
-        baton->error_message = strdup(uv_strerror(-errno));
+        detect_req->error_message = strdup(uv_strerror(-errno));
 # endif
 #endif
-      } else if (baton->source_is_path) {
-        if (magic_load(magic, baton->magic_source) == -1
+      } else if (detect_req->source_is_path) {
+        if (magic_load(magic, detect_req->magic_source) == -1
             && magic_load(magic, fallbackPath) == -1) {
-          baton->error_message = strdup(magic_error(magic));
+          detect_req->error_message = strdup(magic_error(magic));
           magic_close(magic);
           magic = nullptr;
         }
       } else if (magic_load_buffers(magic,
-                                    (void**)&baton->magic_source,
-                                    &baton->source_len,
+                                    (void**)&detect_req->magic_source,
+                                    &detect_req->source_len,
                                     1) == -1) {
-        baton->error_message = strdup(magic_error(magic));
+        detect_req->error_message = strdup(magic_error(magic));
         magic_close(magic);
         magic = nullptr;
       }
 
-      if (magic == nullptr) {
-        if (baton->error_message)
-          baton->error = true;
+      if (magic == nullptr)
         return;
-      }
 
-      if (baton->data_is_path) {
+      if (detect_req->data_is_path) {
 #ifdef _WIN32
         // open the file manually to help cope with potential unicode characters
         // in filename
-        const char* ofn = baton->data;
+        const char* ofn = detect_req->data;
         int flags = O_RDONLY | O_BINARY;
         int fd = -1;
         int wLen;
@@ -270,29 +280,28 @@ public:
           }
         }
         if (fd == -1) {
-          baton->error = true;
-          baton->free_error = false;
-          baton->error_message = "Error while opening file";
+          detect_req->free_error = false;
+          detect_req->error_message = "Error while opening file";
           magic_close(magic);
           return;
         }
         result = magic_descriptor(magic, fd);
         _close(fd);
 #else
-        result = magic_file(magic, baton->data);
+        result = magic_file(magic, detect_req->data);
 #endif
       } else {
-        result = magic_buffer(magic, (const void*)baton->data, baton->data_len);
+        result = magic_buffer(magic,
+                              (const void*)detect_req->data,
+                              detect_req->data_len);
       }
 
       if (result == nullptr) {
         const char* error = magic_error(magic);
-        if (error) {
-          baton->error = true;
-          baton->error_message = strdup(error);
-        }
+        if (error)
+          detect_req->error_message = strdup(error);
       } else {
-        baton->result = strdup(result);
+        detect_req->result = strdup(result);
       }
 
       magic_close(magic);
@@ -300,30 +309,28 @@ public:
 
     static void DetectAfter(uv_work_t* req) {
       Nan::HandleScope scope;
-      Baton* baton = static_cast<Baton*>(req->data);
+      DetectRequest* detect_req = static_cast<DetectRequest*>(req->data);
+      Local<Function> callback = Nan::New(detect_req->callback);
+      Local<Object> target = Nan::New<Object>();
 
-      baton->data_buffer.Reset();
-
-      if (baton->error) {
-        Local<Value> err = Nan::Error(baton->error_message);
-
-        if (baton->free_error)
-          free(baton->error_message);
-
+      if (detect_req->error_message) {
+        Local<Value> err = Nan::Error(detect_req->error_message);
         Local<Value> argv[1] = { err };
-        baton->callback->Call(1, argv);
+        detect_req->runInAsyncScope(target, callback, 1, argv);
       } else {
         Local<Value> argv[2];
-        int multi_result_flags = (baton->flags & (MAGIC_CONTINUE | MAGIC_RAW));
+        int multi_result_flags =
+          (detect_req->flags & (MAGIC_CONTINUE | MAGIC_RAW));
 
         argv[0] = Nan::Null();
 
         if (multi_result_flags == (MAGIC_CONTINUE | MAGIC_RAW)) {
           Local<Array> results = Nan::New<Array>();
-          if (baton->result) {
+          if (detect_req->result) {
             uint32_t i = 0;
-            const char* result_end = baton->result + strlen(baton->result);
-            const char* last_match = baton->result;
+            const char* result_end =
+              detect_req->result + strlen(detect_req->result);
+            const char* last_match = detect_req->result;
             const char* cur_match;
             while (true) {
               if (!(cur_match = strstr(last_match, "\n- "))) {
@@ -350,41 +357,34 @@ public:
             }
           }
           argv[1] = Local<Value>(results);
-        } else if (baton->result) {
-          argv[1] = Local<Value>(Nan::New<String>(baton->result).ToLocalChecked());
+        } else if (detect_req->result) {
+          argv[1] =
+            Local<Value>(Nan::New<String>(detect_req->result).ToLocalChecked());
         } else  {
           argv[1] = Local<Value>(Nan::New<String>().ToLocalChecked());
         }
 
-        if (baton->result)
-          free((void*)baton->result);
-
-        baton->callback->Call(2, argv);
+        detect_req->runInAsyncScope(target, callback, 2, argv);
       }
 
-      if (baton->data_is_path)
-        free(baton->data);
-      delete baton->callback;
-      delete baton;
+      delete detect_req;
     }
 
     static void SetFallback(const Nan::FunctionCallbackInfo<v8::Value>& args) {
-
       if (fallbackPath)
         free((void*)fallbackPath);
 
-      if (args.Length() > 0 && args[0]->IsString()
-          && args[0]->ToString()->Length() > 0) {
-        String::Utf8Value str(args[0]->ToString());
-        fallbackPath = strdup((const char*)(*str));
-      } else {
-        fallbackPath = nullptr;
+      fallbackPath = nullptr;
+      if (args.Length() > 0 && args[0]->IsString()) {
+        Nan::Utf8String str(args[0]);
+        if (str.length() > 0)
+          fallbackPath = strdup((const char*)(*str));
       }
 
       return args.GetReturnValue().Set(args.This());
     }
 
-    static void Initialize(Handle<Object> target) {
+    static void Initialize(Local<Object> target) {
 
       Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
 
@@ -393,16 +393,21 @@ public:
       Nan::SetPrototypeMethod(tpl, "detectFile", DetectFile);
       Nan::SetPrototypeMethod(tpl, "detect", Detect);
 
-      constructor.Reset(tpl->GetFunction());
-      target->Set(Nan::New<String>("setFallback").ToLocalChecked(),
-                  Nan::New<FunctionTemplate>(SetFallback)->GetFunction());
+      constructor.Reset(Nan::GetFunction(tpl).ToLocalChecked());
+      Nan::Set(target,
+               Nan::New<String>("setFallback").ToLocalChecked(),
+               Nan::GetFunction(
+                 Nan::New<FunctionTemplate>(SetFallback)
+               ).ToLocalChecked()).FromJust();
 
-      target->Set(Nan::New<String>("Magic").ToLocalChecked(), tpl->GetFunction());
+      Nan::Set(target,
+               Nan::New<String>("Magic").ToLocalChecked(),
+               Nan::GetFunction(tpl).ToLocalChecked()).FromJust();
     }
 };
 
 extern "C" {
-  void init(Handle<Object> target) {
+  void init(Local<Object> target) {
     Nan::HandleScope();
     Magic::Initialize(target);
   }
